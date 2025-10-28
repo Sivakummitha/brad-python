48a8fe64f705a900d72baec1077541869180f29f
Sivakummitha <sivacharan.k@bradsol.in>
2025-10-28 15:24:22 +0530

    DAY-2 PROGRAM

---
+++ pyn31.py
@@ -0,0 +1,36 @@
+#Write a program that includes all types of loops - for, while, do while pass, break and continue
+print("For Loop")
+for i in range(1, 6):
+    print(i)
+
+print("While Loop")
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+print("Do While Loop")
+x = 1
+while True:
+    print(x)
+    x += 1
+    if x > 5:
+        break
+
+print("Break Example")
+for i in range(1, 10):
+    if i == 5:
+        break
+    print(i)
+
+print("Continue Example")
+for i in range(1, 8):
+    if i == 4:
+        continue
+    print(i)
+
+print("Pass Example")
+for i in range(1, 6):
+    if i == 3:
+        pass
+    
