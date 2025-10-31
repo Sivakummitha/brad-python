1b33c47adb6d1cc4ec9de4f0465614d6cfc6e8cf
Sivakummitha <sivacharan.k@bradsol.in>
2025-10-31 12:41:44 +0530

    Update request_api.py

--- request_api.py
+++ request_api.py
@@ -2,3 +2,7 @@ import requests
 response=requests.get("https://api.github.com")
 print(response.status_code)
 print(response.json())
+urls=["https://api.github.com","https://api.github.com/unknown"]
+for url in urls:
+    res=requests.get(url)
+    print(url,res.status_code)
