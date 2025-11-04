c4a619af746044e065429a40b20809399fd8cba2
Sivakummitha <sivacharan.k@bradsol.in>
2025-11-04 13:00:53 +0530

    module and packages

---
+++ my_package/main.py
@@ -0,0 +1,9 @@
+from my_package import module1, module2
+
+x = 10
+y = 5
+
+print("Addition:", module1.add(x, y))
+print("Subtraction:", module1.subtract(x, y))
+print("Multiplication:", module2.multiply(x, y))
+print("Division:", module2.divide(x, y))
