1fcc3f80e32d1ebcf1ed1653b8c2c05e6a3465f6
Sivakummitha <sivacharan.k@bradsol.in>
2025-11-10 15:54:31 +0530

    Create pyn120.py

---
+++ pyn120.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr = np.array([1.2, 3.7, -2.8, 4.5, -1.1])
+floor_values = np.floor(arr)
+ceil_values = np.ceil(arr)
+trunc_values = np.trunc(arr)
+print("floor values:",floor_values)
+print('ciel values:',ceil_values)
+print("trunc values:",trunc_values)
