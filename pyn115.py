cc32b6610cf5e5f392f731283f0ca57d3b62f338
Sivakummitha <sivacharan.k@bradsol.in>
2025-11-10 15:16:50 +0530

    Create pyn115.py

---
+++ pyn115.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([10,39,29,30,45,26,12])
+mean=np.mean(arr)
+variance=np.var(arr)
+standard_deviation=np.std(arr)
+print("mean:",mean)
+print("varience:",variance)
+print("standard_deviation:",standard_deviation)
