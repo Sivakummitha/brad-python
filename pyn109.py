b22194476d08183fa22ba0f1af3ba02d58e36c1c
Sivakummitha <sivacharan.k@bradsol.in>
2025-11-10 13:11:46 +0530

    109th program

---
+++ pyn109.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+folder_path = "my_folder"
+output_zip = "my_folder_backup"
+if os.path.exists(folder_path) and os.path.isdir(folder_path):
+    shutil.make_archive(output_zip, 'zip', folder_path)
+    print("Folder successfully converted to ZIP file!")
+else:
+    print("Folder not found in the current directory:", os.getcwd())
