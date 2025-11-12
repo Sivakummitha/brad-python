4f8722fd59a729a5adf1d2f670065ae9737d3a45
Sivakummitha <sivacharan.k@bradsol.in>
2025-11-12 14:13:07 +0530

    Create pyn148.py

---
+++ pyn148.py
@@ -0,0 +1,7 @@
+name="sivacharan"
+age=21
+height=6.0
+print(f"my name is {name},age is{age} and my height{height}")
+print("{0} is learning {1}".format("sivacharan", "python"))
+print("The {person} jumped over the {object}.".format(person="sivacharan", object="table"))
+
