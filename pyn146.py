4c6c65bedca23cf922e712a1433f86016c395c67
Sivakummitha <sivacharan.k@bradsol.in>
2025-11-12 13:43:24 +0530

    Create pyn146.py

---
+++ pyn146.py
@@ -0,0 +1,5 @@
+import shutil
+source = "C:\Users\whynew.in\OneDrive\Documents\GitHub\brad-python\pyn.py"
+destination = "C:\Users\whynew.in\OneDrive\Documents\GitHub\brad-python\pyn2.py"
+shutil.copy(source, destination)
+print("File copied successfully!")
