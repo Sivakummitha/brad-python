31ac599bbcee5bdeb11684edd2c0978a88641f5f
Sivakummitha <sivacharan.k@bradsol.in>
2025-11-17 18:46:17 +0530

    Create k.py

---
+++ k.py
@@ -0,0 +1,41 @@
+s = "djanGO with MySQL"
+print("Value is :",s)
+print()
+
+res1 = s.title()
+
+print("Resulted Value is :",res1)
+res2=s.capitalize()
+print("resulted value is:",res2)
+s = "Birla Mandir    "
+print("Value is :",s)
+print("Length is :",len(s))
+print()
+
+res = s.lstrip()
+
+print("Resulted Value is :",res)
+print("Length is :",len(res))
+res2=s.rstrip()
+print(res2)
+s = "Software Developer in TCS"
+print("Value is :",s)
+print("Length is :",len(s))
+print()
+
+res = s.find("o") 
+
+print("Indexing Value is :",res)
+
+print()
+s = "JavaScript"
+print("Value is :",s)
+print("Length is :",len(s))
+print()
+
+res = s.isalpha()
+
+print("using isalpha() :",res)
+
+print()
+
